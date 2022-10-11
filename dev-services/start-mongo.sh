e3c1cb4e0ba148199624087781b53c83ad4a6618
Javier <javiferreyra97@gmail.com>
2022-10-11 19:37:04 -0300

    fix mongodb auth username and password env vars

--- dev-services/start-mongo.sh
+++ dev-services/start-mongo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 docker run -d \
--e "MONGODB_INITDB_ROOT_USERNAME=sa" \
--e "MONGODB_INITDB_ROOT_PASSWORD=M&pa55w0rd" \
+-e "MONGO_INITDB_ROOT_USERNAME=sa" \
+-e "MONGO_INITDB_ROOT_PASSWORD=M&pa55w0rd" \
 -p 27017:27017 \
 -v $PWD/ticken-db/mongo:/data/db \
 --name ticken-mongo \
