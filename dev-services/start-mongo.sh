87a5fd94c709925153e9e587383bd79db63236a2
facundotorraca <facundo@innovid.com>
2022-10-29 02:23:14 -0300

    fix mongodb credential environment vars

--- dev-services/start-mongo.sh
+++ dev-services/start-mongo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 docker run -d \
--e "MONGO_INITDB_ROOT_USERNAME=sa" \
--e "MONGO_INITDB_ROOT_PASSWORD=M&pa55w0rd" \
+-e "MONGO_INITDB_ROOT_USERNAME=admin" \
+-e "MONGO_INITDB_ROOT_PASSWORD=admin" \
 -p 27017:27017 \
 -v $PWD/ticken-db/mongo:/data/db \
 --name ticken-mongo \
