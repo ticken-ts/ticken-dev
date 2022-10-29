fbff4984473c77e760f036feed715405fd9ae544
facundotorraca <facundo@innovid.com>
2022-10-29 02:22:54 -0300

    set fabric binaries on path when starting the service

--- dev-services/start-pvtbc.sh
+++ dev-services/start-pvtbc.sh
@@ -1,30 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
 function getFullPath() {
     relativePath=$1
     echo "$(cd "$(dirname "$relativePath")"; pwd)/$(basename "$relativePath")"
 }
 
-
 cd ../test-pvtbc/test-network
+
+# set bin path
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
 
 # up the network using couchbase
 # as a main database for the peer
-./network.sh up -s couchbase
+sh network.sh up -s couchbase
 
 # create a channel
-./network.sh createChannel -c ticken-channel
+sh network.sh createChannel -c ticken-channel 
 
-deploy a event chaincode
-./network.sh deployCC \
+# deploy a event chaincode
+sh network.sh deployCC \
 -ccn ticken-event \
 -ccp $(getFullPath $PWD/../../../ticken-chaincodes/ticken-event-chaincode) \
 -ccl go \
 -c ticken-channel
 
-
 # deploy a ticket chaincode
-./network.sh deployCC \
+sh network.sh deployCC \
 -ccn ticken-ticket \
 -ccp $(getFullPath $PWD/../../../ticken-chaincodes/ticken-ticket-chaincode) \
 -ccl go \
