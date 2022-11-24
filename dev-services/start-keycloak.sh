5091c70adbacf0dfe70f6ab677e83b974efe4f9e
facundotorraca <facundo@innovid.com>
2022-11-24 01:16:22 -0300

    start keycloak in detached mode

--- dev-services/start-keycloak.sh
+++ dev-services/start-keycloak.sh
@@ -1,4 +1,4 @@
-docker run --name ticken-keycloack -p 8080:8080 \
+docker run -d --name ticken-keycloack -p 8080:8080 \
 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
 quay.io/keycloak/keycloak:latest \
 start-dev
