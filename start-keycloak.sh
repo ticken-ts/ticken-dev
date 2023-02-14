163ad22d4cad9f729afcb7be601339e4b2e87651
Javier <javiferreyra97@gmail.com>
2023-02-13 22:16:30 -0300

    rename keycloak themes folder and fix script typo

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -1,9 +1,9 @@
 docker run \
 -d \
---name ticken-keycloack \
+--name ticken-keycloak \
 -p 8080:8080 \
 -e KEYCLOAK_ADMIN=admin \
 -e KEYCLOAK_ADMIN_PASSWORD=admin \
--v "$PWD/themes":"/opt/keycloak/themes" \
+-v "$PWD/keycloak-themes":"/opt/keycloak/themes" \
 quay.io/keycloak/keycloak:latest \
 start-dev
