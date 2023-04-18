d2b936fefd9a9fdcd21bf7e0953f2eda7d469eb5
facundotorraca <ftorraca@fi.uba.ar>
2023-04-18 01:32:19 -0300

    update ethereum start script to generate 5 deterministic wallets

--- start-ethereum.sh
+++ start-ethereum.sh
@@ -1,1 +1,1 @@
-ganache --wallet.deterministic --wallet.totalAccounts 2 --database.dbPath ./ethereum-db --logging.debug true --logging.verbose true
+ganache --wallet.deterministic --wallet.totalAccounts 2
