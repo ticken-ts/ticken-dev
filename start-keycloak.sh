d871761ac45a3e8fbff9d0eff4c1c3b8761fc634
facundotorraca <ftorraca@fi.uba.ar>
2023-04-18 01:32:37 -0300

    update keycloak start script to apply terraform changes

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -7,3 +7,16 @@ docker run \
 -v "$PWD/keycloak/keycloak-themes":"/opt/keycloak/themes" \
 quay.io/keycloak/keycloak:latest \
 start-dev
+
+sleep 30
+
+keycloak_terraform_def="../ticken-infra/keycloak"
+
+rm -f "${keycloak_terraform_def}/terraform.tfstate"
+
+terraform -chdir=${keycloak_terraform_def} apply \
+  -var="keycloak_admin_username=admin" \
+  -var="keycloak_admin_password=admin" \
+  -auto-approve
+
+
