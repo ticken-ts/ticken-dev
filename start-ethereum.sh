62b640ab87646dc2526688e50439f282f2b779cc
facundotorraca <ftorraca@fi.uba.ar>
2023-05-01 10:48:59 -0300

    start dev eth in container instead of local and remove unused scripts

--- start-ethereum.sh
+++ start-ethereum.sh
@@ -1,1 +1,7 @@
-ganache --wallet.deterministic --wallet.totalAccounts 5
+docker run \
+-d \
+--name ticken-dev-eth \
+trufflesuite/ganache:latest \
+--host 0.0.0.0 \
+--wallet.deterministic \
+--wallet.totalAccounts 5 \
---
+++ utils/wipe-dbs.sh
@@ -0,0 +1,1 @@
+docker volume rm $(docker volume ls -qf name=ticken-dev)
